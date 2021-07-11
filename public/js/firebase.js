195bff75e6588783fa784c089e4102afc609be31
gma-amg <70461308+gma-amg@users.noreply.github.com>
2021-07-11 18:01:13 +0400

    firebase

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyA7zSb5TZ-qTWOrTCsSszweKE-bPpoRfHU",
+    authDomain: "organizer-4160c.firebaseapp.com",
+    projectId: "organizer-4160c",
+    storageBucket: "organizer-4160c.appspot.com",
+    messagingSenderId: "718342037107",
+    appId: "1:718342037107:web:fe878075c6d74b5bec1406",
+    databaseURL:  "https://organizer-4160c-default-rtdb.firebaseio.com/",
+    measurementId: "G-PR274B2S8G"
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
